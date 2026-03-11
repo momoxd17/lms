d8425aba537adeb65c75a1192a7d99f1fabe7c04
moaaz <moaazeedan@gmail.com>
2026-03-11 16:39:40 +0300

    fix axios baseURL add /api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://lms-787t.onrender.com',
+  baseURL: 'https://lms-787t.onrender.com/api',
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
 });
